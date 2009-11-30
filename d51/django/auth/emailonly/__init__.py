f826c3d5f8889c14f820cec518b34c9a4d1f70bc
Travis Swicegood <development@domain51.com>
2009-11-30 14:28:15 -0600

    finish fleshing out the email only version of the backend auth

--- d51/django/auth/emailonly/__init__.py
+++ d51/django/auth/emailonly/__init__.py
@@ -1,14 +1,18 @@
 import pkg_resources
 pkg_resources.declare_namespace(__name__)
 
+from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.models import User
 
-class EmailOnlyBackend():
+class EmailOnlyBackend(ModelBackend):
     def authenticate(self, username=None, password=None):
         if not username or not password:
             return None
 
-        user = User.objects.get(email=username)
-        if user.check_password(password):
-            return user
+        try:
+            user = User.objects.get(email=username)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return super(EmailOnlyBackend, self).authenticate(username, password)
 
--- d51/django/auth/emailonly/tests/emailonly.py
+++ d51/django/auth/emailonly/tests/emailonly.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.models import User
 from django.test import TestCase
 from d51.django.auth.emailonly import EmailOnlyBackend
@@ -6,13 +7,23 @@ __all__ = [
     'TestOfEmailOnlyAuthentication',
 ]
 
-def generate_travis():
+def generate_email_only_user():
     travis = User.objects.create(email='travis@example.com')
     travis.set_password('foobar')
     travis.save()
     return travis
 
+def generate_username_only_user():
+    chris = User.objects.create(username='chris')
+    chris.set_password('foobar')
+    chris.save()
+    return chris
+
 class TestOfEmailOnlyAuthentication(TestCase):
+    def test_is_a_subclass_of_ModelBackend(self):
+        auth = EmailOnlyBackend()
+        self.assert_(isinstance(auth, ModelBackend))
+
     def test_returns_none_if_nothing_provided(self):
         auth = EmailOnlyBackend()
         self.assertEqual(None, auth.authenticate())
@@ -22,7 +33,7 @@ class TestOfEmailOnlyAuthentication(TestCase):
         self.assertEqual(None, auth.authenticate(username='travis@example.com'))
 
     def test_returns_user_if_found(self):
-        travis = generate_travis()
+        travis = generate_email_only_user()
         auth = EmailOnlyBackend()
         user = auth.authenticate(username = 'travis@example.com', password='foobar')
 
@@ -32,8 +43,20 @@ class TestOfEmailOnlyAuthentication(TestCase):
         travis.delete()
 
     def test_returns_none_if_password_does_not_match(self):
-        travis = generate_travis()
+        travis = generate_email_only_user()
         auth = EmailOnlyBackend()
         user = auth.authenticate(username='travis@example.com', password='barfoo')
 
         self.assertEqual(None, user)
+        travis.delete()
+
+    def test_returns_none_if_user_not_found(self):
+        auth = EmailOnlyBackend()
+        self.assertEqual(None, auth.authenticate(username='unknown@example.com', password='foobar'))
+
+    def test_falls_back_on_real_username_if_email_not_found(self):
+        chris = generate_username_only_user()
+        auth = EmailOnlyBackend()
+        user = auth.authenticate(username='chris', password='foobar')
+        self.assertEqual(user, chris)
+
