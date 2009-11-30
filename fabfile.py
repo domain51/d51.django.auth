68434294a55c7da6a3c6dba75ee1ad40d7d0c074
Travis Swicegood <development@domain51.com>
2009-11-30 14:37:45 -0600

    add init command to fab script

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,10 @@
 from fabric.api import *
 import os
 
+def init():
+    local("bin/buildout")
+    local("rm -rf ./project")
+
 def test():
     if os.path.exists("./project-for-testing"):
         local("mv ./project-for-testing ./project")
