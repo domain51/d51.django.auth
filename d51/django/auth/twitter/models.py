c52d11da73c0a1c21b06d8653a581abe4abc6f21
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-01-05 14:13:42 -0600

    added a get_oauth_token method to the TwitterToken model

--- d51/django/auth/twitter/models.py
+++ d51/django/auth/twitter/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.models import User
+from oauth.oauth import OAuthToken
 from django.db import models
 from .managers import TwitterTokenManager
 
@@ -10,3 +11,6 @@ class TwitterToken(models.Model):
     secret = models.CharField(max_length=100)
 
     objects = TwitterTokenManager()
+
+    def get_oauth_token(self):
+        return OAuthToken(self.key, self.secret)
